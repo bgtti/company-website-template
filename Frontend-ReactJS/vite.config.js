5c0c1f15fa8124279ccc642a5d23ac7a2c45647c
bgtti <bru.mgtti@gmail.com>
2026-06-22 00:27:18 +0200

    github workflow and vite and json configs

--- Frontend-ReactJS/vite.config.js
+++ Frontend-ReactJS/vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// the base matches the repository name. If hosting with own domain, change to: base: '/'
 export default defineConfig({
   plugins: [react()],
+  base: '/company-website-template/'
 })
